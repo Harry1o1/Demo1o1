bd61265a254a9430971898366d0e887fed11a065
ajju40959@gmail.com <ajju40959@gmail.com>
2022-03-30 02:51:42 +0600

    Moralis try1o2

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { motion } from 'framer-motion';
-
+import { useMoralis } from "react-moralis";
 
 
 
